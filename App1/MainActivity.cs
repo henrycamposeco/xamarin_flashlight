580a8245080568b0951e0cb04c1ad82679e812fc
Henry Reyes <henrycamposeco@gmail.com>
2017-03-08 16:28:07 -0600

    Add files via upload

---
+++ App1/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Lamp.Plugin;
+using Android.Views;
+
+namespace Flashlight
+{
+    [Activity(Label = "Flashlight", MainLauncher = true, Icon = "@drawable/icon", Theme ="@android:style/Theme.Holo.NoActionBar.Fullscreen")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            base.OnCreate(bundle);
+            base.RequestedOrientation = Android.Content.PM.ScreenOrientation.Portrait;
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            CrossLamp.Current.TurnOn();
+            ToggleButton toggle = FindViewById<ToggleButton>(Resource.Id.Toggle);
+            toggle.Text = "";
+            toggle.Checked = true;
+            toggle.SetBackgroundResource(Resource.Drawable.On);
+            
+
+            toggle.Click += delegate
+            {
+                if (toggle.Checked)
+                {
+                    toggle.SetBackgroundResource(Resource.Drawable.On);
+                    CrossLamp.Current.TurnOn();
+                }
+                else
+                {
+                    toggle.SetBackgroundResource(Resource.Drawable.Off);
+                    CrossLamp.Current.TurnOff();
+                }
+
+            };
+
+
+        }
+    }
+}
+
